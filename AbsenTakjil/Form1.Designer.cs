3f8e341b0c855a36b3b5c2361e18ec68bb823a3d
Athailah, S.Kom, MOS <atha@atha.web.id>
2017-05-30 15:37:38 +0700

    Add project files.

---
+++ AbsenTakjil/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AbsenTakjil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAdministrasi = new System.Windows.Forms.Button();
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.txtPesan1 = new System.Windows.Forms.TextBox();
+            this.txtPesan2 = new System.Windows.Forms.TextBox();
+            this.txtPesan3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.txtBarcode);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(596, 154);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AbsenTakjil.Properties.Resources.pointing;
+            this.pictureBox1.Location = new System.Drawing.Point(251, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBarcode.Location = new System.Drawing.Point(17, 96);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(555, 37);
+            this.txtBarcode.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Scan barcode kamu disini:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.txtPesan3);
+            this.panel2.Controls.Add(this.txtPesan2);
+            this.panel2.Controls.Add(this.txtPesan1);
+            this.panel2.Location = new System.Drawing.Point(8, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(596, 375);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnAdministrasi
+            // 
+            this.btnAdministrasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdministrasi.Location = new System.Drawing.Point(622, 31);
+            this.btnAdministrasi.Name = "btnAdministrasi";
+            this.btnAdministrasi.Size = new System.Drawing.Size(150, 50);
+            this.btnAdministrasi.TabIndex = 2;
+            this.btnAdministrasi.Text = "Administrasi";
+            this.btnAdministrasi.UseVisualStyleBackColor = true;
+            this.btnAdministrasi.Click += new System.EventHandler(this.btnAdministrasi_Click);
+            // 
+            // btnTutup
+            // 
+            this.btnTutup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTutup.Location = new System.Drawing.Point(622, 87);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(150, 50);
+            this.btnTutup.TabIndex = 3;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
+            this.btnTutup.Click += new System.EventHandler(this.btnTutup_Click);
+            // 
+            // txtPesan1
+            // 
+            this.txtPesan1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtPesan1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPesan1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesan1.ForeColor = System.Drawing.Color.Yellow;
+            this.txtPesan1.Location = new System.Drawing.Point(17, 22);
+            this.txtPesan1.Name = "txtPesan1";
+            this.txtPesan1.Size = new System.Drawing.Size(555, 37);
+            this.txtPesan1.TabIndex = 2;
+            this.txtPesan1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPesan2
+            // 
+            this.txtPesan2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtPesan2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPesan2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesan2.ForeColor = System.Drawing.Color.Yellow;
+            this.txtPesan2.Location = new System.Drawing.Point(17, 72);
+            this.txtPesan2.Name = "txtPesan2";
+            this.txtPesan2.Size = new System.Drawing.Size(555, 37);
+            this.txtPesan2.TabIndex = 3;
+            this.txtPesan2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPesan3
+            // 
+            this.txtPesan3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtPesan3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPesan3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesan3.ForeColor = System.Drawing.Color.Yellow;
+            this.txtPesan3.Location = new System.Drawing.Point(17, 122);
+            this.txtPesan3.Name = "txtPesan3";
+            this.txtPesan3.Size = new System.Drawing.Size(555, 37);
+            this.txtPesan3.TabIndex = 4;
+            this.txtPesan3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.btnAdministrasi);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Absen Takjil dan Makan Siang";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAdministrasi;
+        private System.Windows.Forms.Button btnTutup;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPesan2;
+        private System.Windows.Forms.TextBox txtPesan1;
+        private System.Windows.Forms.TextBox txtPesan3;
+    }
+}
+
